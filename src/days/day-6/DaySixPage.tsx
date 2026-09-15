912001e4017032daabe600161ec5815280eda530
Stuart Chen <stuartchen.dev@gmail.com>
2026-09-15 12:01:41 +0800

    feat(day-6): create day 6 scaffold environment

---
+++ src/days/day-6/DaySixPage.tsx
@@ -0,0 +1,5 @@
+export function DaySixPage(){
+    return(
+        <p>Day 6</p>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { createSprintRoutes } from './app/routes'
-import { DayFivePage } from './days/day-5/DayFivePage'
 import { sprintDays } from './days/sprintDays'
+import {DaySixPage} from './days/day-6/DaySixPage'
 import './app/app.css'
 
 const router = createBrowserRouter(
   createSprintRoutes(sprintDays, [
     {
-      path: '/day-5',
-      element: <DayFivePage />,
+      path: '/day-6',
+      element: <DaySixPage />,
     },
   ]),
 )
